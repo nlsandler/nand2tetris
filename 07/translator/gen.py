3f938460ab54be3ae218d889e9d704bb24b59550
Nora Sandler <nlsandler@gmail.com>
2017-11-08 20:58:21 -0500

    refactor repeated assembly snippets in gen

--- 07/translator/gen.py
+++ 07/translator/gen.py
@@ -36,6 +36,43 @@ PUSH_D_INSTRUCTIONS = """
     M=M+1
 """
 
+#instructions to load value at segment[index] into D
+#for directly memory-mapped segments (pointer, temp), {reg} is A
+#for segments where we only store base ptr (local, argument, this, that),
+#we need to dereference that pointer, so {reg} is M 
+LOAD_INTO_D_INSTRUCTIONS = """
+//push {segment} {index}
+    @{base}
+    D={reg} //D holds base address of segment
+    @{index}
+    A=D+A //A holds address of value we want to push
+    D=M //D holds value we want to push"""
+
+#instructions to pop value into segment[index]
+#for directly memory-mapped segments (pointer, temp), {reg} is A
+#for segments where we only store base ptr (local, argument, this, that),
+#we need to dereference that pointer, so {reg} is M 
+POP_INSTRUCTIONS = """
+//pop {segment} {index}
+
+//calculate value we want to pop to, store in R13
+    @{base}
+    D={reg} //D holds base address of segment
+    @{index}
+    D=D+A //D holds address we want to write to
+    @R13
+    M=D //store address to write to in R13
+
+//pop top of stack into D
+    @SP
+    AM=M-1
+    D=M //D holds top value on stack
+
+//write D to address stored in R13
+    @R13
+    A=M
+    M=D"""
+
 #instructions to pop top of stack into D and decrement SP
 
 class Writer:
@@ -93,86 +130,57 @@ class Writer:
         self.f.write(instructions+"\n")
 
 
-    def write_push_pop(self, command:  parse.Command) -> None:
-        if command.cmd_type == parse.CmdType.PUSH:
-            #first generate code to get value into D
-            if command.arg1 == "constant":
-                instructions = """//push constant {c}
+    def write_push(self, command: parse.Command) -> None:
+        #first generate code to get value into D
+
+        if command.arg1 == "constant":
+            instructions = """//push constant {c}
     @{c}
     D=A""".format(c=command.arg2)
-            elif command.arg1 in BASE_PTRS:
-                reg = BASE_PTRS[command.arg1]
-                instructions = """//push {segment} {index}
-    @{base}
-    D=M //D holds base address of segment
-    @{index}
-    A=D+A //A holds address of value we want to push
-    D=M //D holds value we want to push""".format(index=command.arg2, segment=command.arg1, base=reg)
+        elif command.arg1 in BASE_PTRS:
+            base = BASE_PTRS[command.arg1]
+            instructions = LOAD_INTO_D_INSTRUCTIONS.format(index=command.arg2, 
+                                                                segment=command.arg1, 
+                                                                base=base, reg="M")
+        elif command.arg1 in BASE_VALS:
+            base = BASE_VALS[command.arg1]
+            instructions = LOAD_INTO_D_INSTRUCTIONS.format(index=command.arg2, 
+                                                                segment=command.arg1, 
+                                                                base=base, reg="A")
+        #Now push value in D onto stack
+        instructions += PUSH_D_INSTRUCTIONS
 
-            elif command.arg1 in BASE_VALS:
-                base = BASE_VALS[command.arg1]
-                instructions = """//push {segment} {index}
-    @{base}
-    D=A
-    @{index}
-    A=D+A //A holds address of value we want to push
-    D=M //D holds value we want to push""".format(index=command.arg2, segment=command.arg1, base=base)
+        self.f.write(instructions+"\n")
 
+    def write_pop(self, command: parse.Command) -> None:
+        if command.arg1 in BASE_PTRS:
+            base = BASE_PTRS[command.arg1]
+            instructions = POP_INSTRUCTIONS.format(index=command.arg2,
+                                                        segment=command.arg1,
+                                                        base=base, reg="M")
 
-            #Now push value in D onto stack
-            instructions += PUSH_D_INSTRUCTIONS
+        elif command.arg1 in BASE_VALS:
+            base = BASE_VALS[command.arg1]
+            instructions = POP_INSTRUCTIONS.format(index=command.arg2,
+                                                        segment=command.arg1,
+                                                        base=base, reg="A") 
+
+        elif command.arg1 == "constant":
+            raise RuntimeError("Cannot pop a constant")              
+
+        else:
+            raise NotImplementedError("segment {}".format(command.arg1))
+
+        self.f.write(instructions+"\n")           
+
+    def write_push_pop(self, command:  parse.Command) -> None:
+        if command.cmd_type == parse.CmdType.PUSH:
+            self.write_push(command)
+
 
         elif command.cmd_type == parse.CmdType.POP:
-            if command.arg1 in BASE_PTRS:
-                reg = BASE_PTRS[command.arg1]
-                instructions = """//pop {segment} {index}
-
-//calculate value we want to pop to, store in R13
-    @{base}
-    D=M //D holds base address of segment
-    @{index}
-    D=D+A //D holds address we want to write to
-    @R13
-    M=D //store address to write to in R13
-
-//pop top of stack into D
-    @SP
-    AM=M-1
-    D=M //D holds top value on stack
-
-//write D to address stored in R13
-    @R13
-    A=M
-    M=D""".format(segment=command.arg1, index=command.arg2, base=reg)
-
-            elif command.arg1 in BASE_VALS:
-                base = BASE_VALS[command.arg1]
-
-                instructions = """//pop {segment} {index}
-
-//calculate value we want to pop to, store in R13
-    @{base}
-    D=A //D holds base address of segment
-    @{index}
-    D=D+A //D holds address we want to write to
-    @R13
-    M=D //store address to write to in R13
-
-//pop top of stack into D
-    @SP
-    AM=M-1
-    D=M //D holds top value on stack
-
-//write D to address stored in R13
-    @R13
-    A=M
-    M=D""".format(segment=command.arg1, index=command.arg2, base=base)                
-
-            else:
-                raise NotImplementedError("segment {}".format(command.arg1))
+            self.write_pop(command)
 
         else:
             raise RuntimeError("Expected command type push or pop, got {}".format(command.cmd_type))
-
-        self.f.write(instructions+"\n")
 
